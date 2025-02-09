fd106c9b574a2da1d180fcc600886238f4a9232f
Sigge Brandt <sigge.brandt567@gmail.com>
2025-02-09 18:43:34 +0100

    testtenta f4

--- 04 Vecka 7/testtentamen/f4 - IdNumber/kodbas.js
+++ 04 Vecka 7/testtentamen/f4 - IdNumber/kodbas.js
@@ -1,10 +1,34 @@
 class Person {
-  static all = []
-  constructor (bornDate, name, gender) {
+  static all = [];
+  constructor(bornDate, name, gender) {
     this.bornDate = bornDate; // {year: int, month: int, day: int}
     this.name = name;
     this.gender = gender;
     this.setIdNumber();
     this.constructor.all.push(this);
+  }
+
+  setIdNumber() {
+    let randomIdNumber = "";
+    let idExists = true;
+
+    while (idExists) {
+      randomIdNumber = "";
+      for (let i = 0; i < 4; i++) {
+        randomIdNumber += Math.floor(Math.random() * 10);
+      }
+
+      idExists = Person.all.some(
+        (person) =>
+          person.bornDate == this.bornDate && person.idNumber == randomIdNumber
+      );
+    }
+    if (this.gender == "f" && randomIdNumber[2] % 2 == 0) {
+      return (this.idNumber = randomIdNumber);
+    } else if (this.gender == "m" && randomIdNumber[2] % 2 != 0) {
+      return (this.idNumber = randomIdNumber);
+    } else {
+      this.setIdNumber();
+    }
   }
 }
